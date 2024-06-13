29cd8c5fa65f153cfb523b0cea7a7ac04672319b
jennier0107 <mt3085570450@outlook.com>
2024-06-13 11:31:18 +0800

    remove sphinx role for del-marker

--- src/sphinxcontrib_del_marker/del_marker.py
+++ src/sphinxcontrib_del_marker/del_marker.py
@@ -4,3 +4,15 @@
 # @Author  MT308
 # @Date    2024/6/9 18:49
 """
+
+from docutils import nodes
+from docutils.parsers.rst import Directive
+
+
+class DelMarkerDirective(Directive):
+    # this enables content in the directive
+    has_content = True
+
+    def run(self):
+        node = nodes.raw('', f'<del>{self.content[0]}</del>', format='html')
+        return [node]
