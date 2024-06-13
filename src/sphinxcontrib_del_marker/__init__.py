1e93b0b82ed02c2fa11f6de82448de4d3d81fa60
jennier0107 <mt3085570450@outlook.com>
2024-06-13 17:07:27 +0800

    use nested_parse and node instread of nodes.raw

--- src/sphinxcontrib_del_marker/__init__.py
+++ src/sphinxcontrib_del_marker/__init__.py
@@ -1,12 +1,13 @@
 from sphinx.application import Sphinx
 from sphinx.util.typing import ExtensionMetadata
 
-from .del_marker import DelMarkerDirective
+from .del_marker import DelMarkerDirective, DelNode, visit_del_node, depart_del_node
 
 __version__ = '0.0.1'
 
 
 def setup(app: Sphinx) -> ExtensionMetadata:
+    app.add_node(DelNode, html=(visit_del_node, depart_del_node))
     app.add_directive("del", DelMarkerDirective)
 
     return {
--- src/sphinxcontrib_del_marker/del_marker.py
+++ src/sphinxcontrib_del_marker/del_marker.py
@@ -9,10 +9,26 @@ from docutils import nodes
 from docutils.parsers.rst import Directive
 
 
+class DelNode(nodes.General, nodes.Element):
+    pass
+
+
+def visit_del_node(self, node):
+    self.body.append(self.starttag(node, 'del'))
+
+
+def depart_del_node(self, node):
+    self.body.append('</del>')
+
+
 class DelMarkerDirective(Directive):
     # this enables content in the directive
     has_content = True
 
     def run(self):
-        node = nodes.raw('', f'<del>{self.content[0]}</del>', format='html')
-        return [node]
+        paragraph_node = nodes.paragraph()
+
+        self.state.nested_parse(self.content, self.content_offset, paragraph_node)
+        del_node_instance = DelNode()
+        del_node_instance += paragraph_node.children
+        return [del_node_instance]
